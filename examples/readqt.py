47c0effd311f1a9f2aff9b282bc8ca60e237e53a
pi4001-01 <sysdcs@gmail.com>
2020-02-24 11:04:29 -0500

    added more stuff

--- examples/readqt.py
+++ examples/readqt.py
@@ -1,12 +1,33 @@
 import sys
+import PyQt5
 from PyQt5.QtCore import Qt
+from PyQt5.QtCore import QThread
 from PyQt5.QtWidgets import (QApplication, QCheckBox, QGridLayout, QGroupBox,
                              QMenu, QPushButton, QRadioButton, QVBoxLayout, QWidget, QSlider)
 
+class DataCaptureThread(QThread):
+    def collectProcessData():
+        print ("Collecting Process Data")
+
+    def __init__(self):
+        QThread.__init__(self)
+        #declaring the timer
+        self.dataCollectionTimer = PyQt5.QtCore.QTimer()
+        self.dataCollectionTimer.timeout.connect(self.collectProcessData)
+    def run(self):
+        self.dataCollectionTimer.start(1000);
+        loop = QEventLoop()
+        loop.exec_()
+        
 class Window(QWidget):
     def __init__(self, parent=None):
         super(Window, self).__init__(parent)
+        self.datacap = DataCaptureThread()
+        self.datacap.run()
+        
+        
 
+        
         grid = QGridLayout()
         grid.addWidget(self.createExampleGroup(), 0, 0)
         grid.addWidget(self.createExampleGroup(), 1, 0)
@@ -17,6 +38,12 @@ class Window(QWidget):
         self.setWindowTitle("PyQt5 Sliders")
         self.resize(400, 300)
 
+    def startThread(self):
+        self.datacap.start()
+
+    def stopThread(self):
+        self.datacap.terminate()
+        
     def createExampleGroup(self):
         groupBox = QGroupBox("Slider Example")
 
@@ -41,5 +68,7 @@ class Window(QWidget):
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     clock = Window()
+    #clock.startThread()
     clock.show()
+    #clock.stopThread()
     sys.exit(app.exec_())
